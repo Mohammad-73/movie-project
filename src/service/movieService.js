6f2acac603cebfdc99fe08698cb9447b04dc6c8b
Mohammad-73 <m.khalili7310@gmail.com>
2021-08-21 20:03:44 +0430

    add favMedia to movieService

--- src/service/movieService.js
+++ src/service/movieService.js
@@ -8,9 +8,21 @@ function rateTv(id, rate) {
   return request.post({ path: `tv/${id}/rating` }, { value: rate });
 }
 
+function favMedia(accountId, mediaType, mediaId, favStatus) {
+  return request.post(
+    { path: `account/${accountId}/favorite` },
+    {
+      media_type: mediaType,
+      media_id: mediaId,
+      favorite: favStatus,
+    }
+  );
+}
+
 const movieService = {
   rateMovie,
   rateTv,
+  favMedia,
 };
 
 export default movieService;
