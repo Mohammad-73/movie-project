d9572cc07855686ab9495d7a7374d315645b865a
Mohammad-73 <m.khalili7310@gmail.com>
2021-08-21 19:44:58 +0430

    add tv rating to movie service

--- src/service/movieService.js
+++ src/service/movieService.js
@@ -1,11 +1,16 @@
 import request from "../helper/request";
 
-function rate(id, rate) {
+function rateMovie(id, rate) {
   return request.post({ path: `movie/${id}/rating` }, { value: rate });
 }
 
+function rateTv(id, rate) {
+  return request.post({ path: `tv/${id}/rating` }, { value: rate });
+}
+
 const movieService = {
-  rate,
+  rateMovie,
+  rateTv,
 };
 
 export default movieService;
